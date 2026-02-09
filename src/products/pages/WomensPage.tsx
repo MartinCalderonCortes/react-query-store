642dfa000973ea11cccfd75a0dd2e0dbd2b368e0
Martin <mh.straus@hotmail.com>
2026-02-09 17:10:02 -0500

    Load products on the womens page component

--- src/products/pages/WomensPage.tsx
+++ src/products/pages/WomensPage.tsx
@@ -1,11 +1,14 @@
-import { ProductList } from ".."
+import { ProductList, useProducts } from ".."
 
 export const WomensPage = () => {
+  const { products, isFetching, error } = useProducts({ filtersOption: "women's clothing" });
   return (
     <div className="flex-col">
       <h1 className="text-2xl font-bold">Productos para mujeres</h1>
 
-      <ProductList />
+      {isFetching && <p>Loading</p>}
+      
+      <ProductList products={products}/>
 
     </div>
   )
