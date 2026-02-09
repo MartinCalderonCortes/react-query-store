5c40507ecbf6806713ca1ccd6f05296057eaa909
Martin <mh.straus@hotmail.com>
2026-02-09 13:16:24 -0500

    Add product as prop, and spreed the product object to set the information of the product on the ProductCard component

--- src/products/components/ProductCard.tsx
+++ src/products/components/ProductCard.tsx
@@ -1,29 +1,29 @@
 import { Card, Image } from "@heroui/react";
+import { Product } from "../interfaces/product";
 
+export const ProductCard = ({ product }: { product: Product}) => {
 
-
-export const ProductCard = () => {
+  const { category, title, description, image, price } = product
   return (
     <Card
       className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
       <div className="w-full md:w-1/3 bg-white grid place-items-center">
-        <Image src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
-          alt="tailwind logo"
+        <Image src={image}
+          alt={title}
           width={300}
-          height={400}
+          height={300}
           className="rounded-xl p-5 sm:p-0 bg-white" />
       </div>
       <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
         <div className="flex justify-between item-center">
-          <p className="text-gray-500 font-medium hidden md:block">Vacations</p>
+          <p className="text-gray-500 font-medium hidden md:block">{ category }</p>
         </div>
-        <h3 className="font-black text-gray-800 md:text-2xl text-xl">The Majestic and Wonderful Bahamas</h3>
+        <h3 className="font-black text-gray-800 md:text-2xl text-xl">{ title }</h3>
 
-        <p className="md:text-lg text-gray-500 text-base">The best kept secret of The Bahamas is the country’s sheer
-          size and diversity. With 16 major islands, The Bahamas is an unmatched destination</p>
+        <p className="md:text-lg text-gray-500 text-base">{ description.slice(0, 50) }...</p>
 
         <p className="text-xl font-black text-gray-800">
-          $110
+          ${ price }
           <span className="font-normal text-gray-600 text-base"> +impuesto</span>
         </p>
       </div>
